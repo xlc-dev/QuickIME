67f97501850cc3e2bc292b2f6add979738055f85
xlc-dev <119954023+xlc-dev@users.noreply.github.com>
2025-06-19 11:47:12 +0200

    oops didnt import css :)

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,13 @@
 import { Router } from "@solidjs/router";
 import { FileRoutes } from "@solidjs/start/router";
-import { ColorModeProvider, ColorModeScript, createLocalStorageManager } from "@kobalte/core";
+import {
+  ColorModeProvider,
+  ColorModeScript,
+  createLocalStorageManager,
+} from "@kobalte/core";
 import { Suspense } from "solid-js";
+
+import "./app.css";
 
 export default function App() {
   const storageManager = createLocalStorageManager("theme");
